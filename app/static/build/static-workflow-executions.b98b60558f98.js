c0f5cdc819bdf222ed24cbef99b4d53fe7ad4639
mmayhew <nona@darklab.sh>
2026-08-07 01:35:50 -0500

    Project Assessment Workspace: Synchronize CI test contracts

---
+++ app/static/build/static-workflow-executions.b98b60558f98.js
@@ -0,0 +1,2 @@
+import{a,b,c,d,e,f,g,h}from"./static-chunk-b3d865c3f385.js";export{d as cancelWorkflowExecution,a as createWorkflowExecution,h as createWorkflowExecutionController,f as formatWorkflowExecutionElapsed,c as getWorkflowExecution,b as listWorkflowExecutions,g as renderWorkflowExecutionsSection,e as workflowExecutionIsActive};
+//# sourceMappingURL=static-workflow-executions.b98b60558f98.js.map
